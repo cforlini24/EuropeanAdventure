94293be7746cf2dd8a90aadb09a089ea47770b73
Chase Forlini <cforlini24@gmail.com>
2024-05-02 23:38:24 -0400

    testing layouts

--- src/Components/Lineups.js
+++ src/Components/Lineups.js
@@ -7,7 +7,7 @@ import "yet-another-react-lightbox/plugins/captions.css";
 
 
 const Lineup = (props) => {
-    const { lineups, setEditSent, editSent,  map, loading } = props;
+    const { lineups, setEditSent, editSent, map, loading } = props;
     console.log(lineups)
 
 
@@ -57,14 +57,14 @@ const Lineup = (props) => {
         }
     }, [grenadeFilter, lineups]);
 
-    useEffect(()=> {
+    useEffect(() => {
         setHitchLoading(true);
-        setTimeout(()=> {
+        setTimeout(() => {
             setHitchLoading(false)
-        },500)
-    },[props])
+        }, 500)
+    }, [props])
 
-    
+
 
 
 
@@ -87,85 +87,91 @@ const Lineup = (props) => {
                     <button className={grenadeFilter == "Flash" ? "btn btn-warning active" : "btn btn-secondary-outline"} onClick={() => setGrenadeFilter("Flash")}>Flash</button>
                 </div>
             </div>
-            {
-                loading || hitchLoading ?
-                    <div className="d-flex justify-content-center mt-5">
-                        <div class="spinner-border" role="status">
-                            <span class="visually-hidden">Loading...</span>
-                        </div>
-                    </div>
-                    : CTSelected ? CTLineups.length ?
-                        CTLineups.map((lineup, i) => {
-                            return (
-                                <div key={lineup._id} className="row mx-auto lineup-container mb-3 border-bottom border-5 border-dark  " >
-                                    <div className="d-flex justify-content-between ">
-                                        <h2>{lineup.type == "HE" ? "High-Explosive" : lineup.type }: {lineup.title}</h2>
-                                        <div>
-                                            <Link to={`/edit/${lineup._id}`} >
-                                                <button className="btn btn-secondary mx-2">Edit</button>
-                                            </Link>
-                                            <button className="btn btn-danger me-5" onClick={() => deleteLineup(lineup._id)}>Delete</button>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-6">
-                                            <img src={lineup.posImage64} alt="Showing where to stand" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
-                                                setOpen(true);
-                                                setLightBoxSrc([{ src: lineup.posImage64, description: lineup.desc },
-                                                { src: lineup.aimImage64, description: lineup.desc }])
-                                            }} />
-                                        </div>
-                                        <div className="col-6 mb-1">
-                                            <img src={lineup.aimImage64} alt="Showing where to aim" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
-                                                setOpen(true);
-                                                setLightBoxSrc([{ src: lineup.aimImage64, description: lineup.desc },
-                                                { src: lineup.posImage64, description: lineup.desc }])
-                                            }} />
-                                        </div>
-                                        <h5>{lineup.desc}</h5>
-                                    </div>
+            <div className="container-fluid w-100">
+
+                <div className="row gy-3 ">
+
+                    {
+                        loading || hitchLoading ?
+                            <div className="d-flex justify-content-center mt-5">
+                                <div class="spinner-border" role="status">
+                                    <span class="visually-hidden">Loading...</span>
                                 </div>
-                            )
-                        }) :
-                        <div className="d-flex justify-content-center mt-5">
-                            <p>No lineups found</p>
-                        </div>
-                        : TLineups.length?
-                            TLineups.map((lineup, i) => {
-                                return (
-                                    <div key={lineup._id} className="row mx-auto lineup-container mb-3 border-bottom border-5 border-dark  " >
-                                        <div className="d-flex justify-content-between ">
-                                        <h2>{lineup.type == "HE" ? "High-Explosive" : lineup.type }: {lineup.title}</h2>
-                                            <div>
-                                                <Link to={`/edit/${lineup._id}`} >
-                                                    <button className="btn btn-secondary mx-2">Edit</button>
-                                                </Link>
-                                                <button className="btn btn-danger me-5" onClick={() => deleteLineup(lineup._id)}>Delete</button>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-6">
-                                                <img src={lineup.posImage64} alt="Showing where to stand" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
-                                                    setOpen(true);
-                                                    setLightBoxSrc([{ src: lineup.posImage64, description: lineup.desc },
-                                                    { src: lineup.aimImage64, description: lineup.desc }])
-                                                }} />
-                                            </div>
-                                            <div className="col-6 mb-1">
-                                                <img src={lineup.aimImage64} alt="Showing where to aim" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
-                                                    setOpen(true);
-                                                    setLightBoxSrc([{ src: lineup.aimImage64, description: lineup.desc },
-                                                    { src: lineup.posImage64, description: lineup.desc }])
-                                                }} />
-                                            </div>
-                                            <h5>{lineup.desc}</h5>
-                                        </div>
-                                    </div>
-                                )
-                            }) : <div className="d-flex justify-content-center mt-5">
-                                <p>No lineups found</p>
                             </div>
-            }
+                            : CTSelected ? CTLineups.length ?
+                                CTLineups.map((lineup, i) => {
+                                    return (
+                                        <div key={lineup._id} className="col-3 mb-3" >
+                                            <div className="d-flex justify-content-between ">
+                                                <h2>{lineup.type == "HE" ? "High-Explosive" : lineup.type}: {lineup.title}</h2>
+                                                <div className="my-2">
+                                                    <Link to={`/edit/${lineup._id}`} >
+                                                        <button className="btn btn-secondary mx-2">Edit</button>
+                                                    </Link>
+                                                    <button className="btn btn-danger my-1" onClick={() => deleteLineup(lineup._id)}>Delete</button>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="mb-1">
+                                                    <img src={lineup.posImage64} alt="Showing where to stand" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
+                                                        setOpen(true);
+                                                        setLightBoxSrc([{ src: lineup.posImage64, description: lineup.desc },
+                                                        { src: lineup.aimImage64, description: lineup.desc }])
+                                                    }} />
+                                                </div>
+                                                <div className=" mb-1">
+                                                    <img src={lineup.aimImage64} alt="Showing where to aim" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
+                                                        setOpen(true);
+                                                        setLightBoxSrc([{ src: lineup.aimImage64, description: lineup.desc },
+                                                        { src: lineup.posImage64, description: lineup.desc }])
+                                                    }} />
+                                                </div>
+                                                <h5>{lineup.desc}</h5>
+                                            </div>
+                                        </div>
+                                    )
+                                }) :
+                                <div className="d-flex justify-content-center mt-5">
+                                    <p>No lineups found</p>
+                                </div>
+                                : TLineups.length ?
+                                    TLineups.map((lineup, i) => {
+                                        return (
+                                            <div key={lineup._id} className="col-4 mb-3" >
+                                            <div className="d-flex justify-content-between ">
+                                                <h2>{lineup.type == "HE" ? "High-Explosive" : lineup.type}: {lineup.title}</h2>
+                                                <div className="my-2">
+                                                    <Link to={`/edit/${lineup._id}`} >
+                                                        <button className="btn btn-secondary mx-2">Edit</button>
+                                                    </Link>
+                                                    <button className="btn btn-danger my-1" onClick={() => deleteLineup(lineup._id)}>Delete</button>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="mb-1">
+                                                    <img src={lineup.posImage64} alt="Showing where to stand" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
+                                                        setOpen(true);
+                                                        setLightBoxSrc([{ src: lineup.posImage64, description: lineup.desc },
+                                                        { src: lineup.aimImage64, description: lineup.desc }])
+                                                    }} />
+                                                </div>
+                                                <div className=" mb-1">
+                                                    <img src={lineup.aimImage64} alt="Showing where to aim" className="img-fluid img" style={{ cursor: "pointer" }} onClick={() => {
+                                                        setOpen(true);
+                                                        setLightBoxSrc([{ src: lineup.aimImage64, description: lineup.desc },
+                                                        { src: lineup.posImage64, description: lineup.desc }])
+                                                    }} />
+                                                </div>
+                                                <h5>{lineup.desc}</h5>
+                                            </div>
+                                        </div>
+                                        )
+                                    }) : <div className="d-flex justify-content-center mt-5">
+                                        <p>No lineups found</p>
+                                    </div>
+                    }
+                </div>
+            </div>
             <Lightbox
                 open={open}
                 close={() => setOpen(false)}
